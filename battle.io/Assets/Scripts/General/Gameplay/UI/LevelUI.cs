0ed68d7a53352d066c6c1a6917933e71ed2431c0
Illusion <66023494+Mimist-Illusionard@users.noreply.github.com>
2023-07-10 17:20:00 +0300

    Change:
    - Level Ui animation

--- battle.io/Assets/Scripts/General/Gameplay/UI/LevelUI.cs
+++ battle.io/Assets/Scripts/General/Gameplay/UI/LevelUI.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,9 +7,13 @@ public class LevelUI : MonoBehaviour
 {
     [SerializeField] private Level _level;
     [SerializeField] private Image _image;
+    [SerializeField] private Transform _parent;
     [SerializeField] private TMP_Text _levelText;
     
     private float _maxAmount;
+    private float _fillDuration = 0.5f;
+    private float _shakeDuration = 0.25f;
+    private Vector3 _shakeScale = new Vector3(0.025f, 0.025f, 0.025f);
 
     private void Start()
     {
@@ -19,7 +24,8 @@ public class LevelUI : MonoBehaviour
 
     private void UpdateExp(float currentValue)
     {
-        _image.fillAmount = currentValue / _maxAmount;
+        _image.DOFillAmount(currentValue / _maxAmount, _fillDuration);
+        _parent.DOPunchScale(_shakeScale, _shakeDuration);
     }
 
     private void UpdateLevel(int currentLevel)
