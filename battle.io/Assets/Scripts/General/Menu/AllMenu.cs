7a53bb30593b0e53e12ceacb66b267b3bed250d2
RestartYC <neo61483@gmail.com>
2023-07-22 22:11:20 +0300

    Add:
     - Menu (in progress)
     - Load/Save Xml in Menu

---
+++ battle.io/Assets/Scripts/General/Menu/AllMenu.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using Ruinum.Core;
+using System.IO;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AllMenu : BaseSingleton<AllMenu>
+{
+    XmlDocument xDoc = new XmlDocument();
+    XmlReader xReader;
+    Setts[] S = { new Setts("Master",0), new Setts("Music", 0) , new Setts("Effects", 0) };
+    protected override void Awake()
+    {
+        base.Awake();
+        try
+        {
+            xDoc.Load("Settings.xml");
+            for (int i = 0; i < S.Length; i++)
+            {
+                S[i].value = LoadXML(S[i].name);
+                Debug.Log("Loaded: " + S[i].name + " = " + S[i].value);
+            }
+        }
+        catch
+        {
+            XmlWriter writer = XmlWriter.Create("Settings.xml");
+            writer.WriteStartElement("Setts");
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                writer.WriteStartElement("Own");
+                writer.WriteAttributeString("name", S[i].name);
+
+                writer.WriteElementString("Nested", "0");
+                writer.WriteEndElement();
+            }
+
+            writer.WriteEndElement();
+
+            writer.Close();
+            xDoc.Load("Settings.xml");
+        }
+    }
+
+    public void Play()
+    {
+        SaveAll();
+        
+    }
+
+    public void Exit()
+    {
+        SaveAll();
+        Application.Quit();
+    }
+
+    public void SaveAll()
+    {
+        for (int i = 0; i < S.Length; i++)
+        {
+            SaveXML(S[i].name, S[i].value);
+        }
+    }
+
+    public void SetMasterValue(Slider slider)
+    {
+        S[0].value = slider.value;
+    }
+
+    public void SetMusicValue(Slider slider)
+    {
+        S[1].value = slider.value;
+    }
+
+    public void SetEffectsValue(Slider slider)
+    {
+        S[2].value = slider.value;
+    }
+
+    public float LoadXML(string s)
+    {
+        // получим корневой элемент
+        XmlElement xRoot = xDoc.DocumentElement;
+        if (xRoot != null)
+        {
+            // обход всех узлов в корневом элементе
+            foreach (XmlElement xnode in xRoot)
+            {
+                if(xnode.Attributes.GetNamedItem("name").Value == s)
+                {
+                    return float.Parse(xnode.FirstChild.InnerText);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public void SaveXML(string s,float x)
+    {
+        XmlElement xRoot = xDoc.DocumentElement;
+        if (xRoot != null)
+        {
+            // обход всех узлов в корневом элементе
+            foreach (XmlElement xnode in xRoot)
+            {
+                if (xnode.Attributes.GetNamedItem("name").Value == s)
+                {
+                    xnode.FirstChild.InnerText = x.ToString();
+                    Debug.Log("Saved: " + xnode.Attributes.GetNamedItem("name").Value + " = " + xnode.FirstChild.InnerText);
+                }
+            }
+        }
+        xDoc.Save(new FileInfo("Settings.xml").FullName);
+    }
+}
+
+class Setts
+{
+    public string name;
+    public float value;
+
+    public Setts(string n,float v)
+    {
+        name = n;
+        value = v;
+    }
+}
