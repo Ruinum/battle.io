f9d719c66936fb33040dd892c74d7c917a4cb57b
Ilya Reznichenko <>
2023-12-21 16:58:43 +0300

    Add:
    - Separate save implementations

---
+++ battle.io/Assets/Scripts/General/BuildType.cs
@@ -0,0 +1,8 @@
+﻿public enum BuildType
+{
+    None    = 0,
+    Desktop      = 1,
+    Console = 2,
+    WEBGL   = 3,
+    Custom  = 4
+}
---
+++ battle.io/Assets/Scripts/General/Save/DesktopSave.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class DesktopSave : ISave
+{
+    private const string FILE_NAME = "Save.data";
+
+    public void Save(string saveData)
+    {
+        string destination = Application.dataPath + FILE_NAME;
+
+        FileStream file;
+
+        if (File.Exists(destination)) file = File.Open(destination, FileMode.Truncate);
+        else file = File.Create(destination);
+
+        StreamWriter stream = new StreamWriter(file);
+
+        stream.WriteLine(saveData);
+        stream.Close();
+    }
+
+    public bool Load(out string savedData)
+    {
+        var destination = Application.dataPath + FILE_NAME;
+        savedData = "";
+
+        FileStream file;
+        if (File.Exists(destination)) file = File.OpenRead(destination);
+        else return false;
+
+        StreamReader stream = new StreamReader(file);
+        savedData = stream.ReadToEnd();
+        stream.Close();
+
+        return true;       
+    }
+}
